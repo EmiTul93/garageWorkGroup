b720042c169cf23adfe29eb871f592b8aed708f2
TonyF <axelfiumano@gmail.com>
2022-11-09 17:27:32 +0300

    Added:class Furgone

--- src/Start.java
+++ src/Start.java
@@ -1,3 +1,4 @@
+import Veicolo.Furgone;
 import Veicolo.Macchina;
 import Veicolo.Moto;
 
@@ -7,6 +8,10 @@ public class Start {
         Macchina macchina1 = new Macchina("Peugeout",2012,1.2,"diesel", 5);
         Macchina macchina2 = new Macchina("Fiat",2018,1.6,"diesel", 5);
         Macchina macchina3 = new Macchina("Mercedes",2020,3.2,"benzina", 5);
+
+        Furgone furgone1 = new Furgone("Iveco",2000,2.5,2000);
+        Furgone furgone2 = new Furgone("Mercedes",2010,3.0,2500);
+        Furgone furgone3 = new Furgone("Ford",2020,2.9,2500);
 
 
         Moto moto1 = new Moto ("Honda",2020,600, 2);
---
+++ src/Veicolo/Furgone.java
@@ -0,0 +1,23 @@
+package Veicolo;
+
+public class Furgone extends VeicoloAMotore{
+    private int capacita;
+
+    public Furgone(String marca,int anno,double cilindrata,int capacita){
+        super(marca,anno,cilindrata);
+        this.capacita = capacita;
+    }
+    @Override
+    public String getInfoVeicoli() {
+        return super.getInfoVeicoli()+" capacita: "+capacita+" kg ";
+    }
+
+    public int getCapacita(){
+        return capacita;
+    }
+
+    public void setCapacita(int capacita){
+        this.capacita = capacita;
+    }
+
+}

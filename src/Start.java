285b4742be2a2b1a2201a738a424d34c421172dd
Emidio ErMeglio <you@example.com>
2022-11-09 16:02:22 +0200

    firstCommit

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+import Veicolo.Macchina;
+
+public class Start {
+
+    public static void main(String[] args) {
+        Macchina macchina1 = new Macchina("Peugeout",2012,1.2,"diesel", 5);
+        Macchina macchina2 = new Macchina("Fiat",2018,1.6,"diesel", 5);
+        Macchina macchina3 = new Macchina("Mercedes",2020,3.2,"benzina", 5);
+    }
+}
